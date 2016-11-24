cfa4edbce3d0fe670a2a2080e402430921e96ed3
Hugo pereira <ee11167@gnomo.fe.up.pt>
2016-11-24 13:59:26 +0000

    First commit

---
+++ main/main.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Music Addicted</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+</head>
+
+<body>
+	<div id="header">
+		<h1>Music Addicted</h1>
+		<h2>We bring music to you!</h2>
+<!--		<img src="https://www.askideas.com/media/13/Addicted-To-Music.jpg" alt="20x20"> //-->
+	</div>
+
+	<div id="options">
+		<ul>
+			<li><a href="">Sign up</a></li>
+			<li><a href="">Login</a></li>
+		</ul>
+	</div>
+
+	<div id="welcome">
+			<section class="register-benefits">
+				<img src="https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB0PGGM81.png?v=1.0,ff7700">
+					<h3>Welcome to Music Addicted</h3>
+					<ul>
+							<li>The Ability to Rate Albums</li>
+							<li>Album Recommendations Based on Your Taste</li>
+							<li>Create Custom Album Lists</li>
+							<li>A User Profile Where You Can Share Your Lists</li>
+							<li>Follow Artists</li>
+							<li>Keep Track of Your Music Collection</li>
+					</ul>
+			</section>
+	<h3></h3>
+
+	<span class="sibd">Sistemas Informação de Bases de Dados. <br/> Grupo 1</span>
+</div>
+
+<div id="footer">
+	<ul>
+		<li><a href="">About us</a></li>
+		<li><a href="">Contact</a></li>
+		<li><a href="">Help</a></li>
+	</ul>
+</body>
+</html>
+
+<!--<div class="registration-container">
+
+            <section class="register-benefits">
+                <h2>Benefits to signing up for an AllMusic Account include:</h2>
+                <ul>
+                    <li>The Ability to Rate Albums</li>
+                    <li>Album Recommendations Based on Your Taste</li>
+                    <li>Create Custom Album Lists</li>
+                    <li>A User Profile Where You Can Share Your Lists</li>
+                    <li>Follow Artists for New Release Notifications</li>
+                    <li>Keep Track of Your Music Collection</li>
+                    <li>Sign Up for our Weekly New Release Newsletter</li>
+                    <li>Become a Paid Subscriber for an Ad-Free AllMusic</li>
+                </ul>
+            </section>
